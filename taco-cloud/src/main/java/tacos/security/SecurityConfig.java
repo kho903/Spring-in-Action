7f52c2efaa4cedfab1676f2a536e524aab2d9ec8
Kim Ji Hun <gmldnr2222@naver.com>
2023-01-01 15:31:57 +0900

    feat: SecurityConfig

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -30,11 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/design", "/orders")
-					.access("hasRole('ROLE_USER')")
-				.antMatchers("/", "/**").access("permitAll()")
-			.and()
-				.httpBasic();
+			.antMatchers("/design", "/orders")
+			.hasRole("ROLE_USER")
+			.antMatchers("/", "/**").permitAll();
 	}
 
 	@Override
